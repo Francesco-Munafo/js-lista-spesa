e56637b3ab618e713e5e1e7d17ef8499215f47a2
Francesco-Munafo <acquamarea1@gmail.com>
2023-08-30 15:01:52 +0200

    Add js script to print shopping list in html

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione
 
 */
 
-const shoppingList = ["Pasta", "Guanciale", "Pecorino", "Uova", "Pepe nero"]
+// Creo un array per la lista della spesa, nel mio caso gli ingredienti per fare la carbonara
+const shoppingList = ["Pasta", "Guanciale", "Pecorino", "Uova", "Pepe nero"];
+//console.log(shoppingList);
+//console.log(shoppingList.length);
+
+// Creo una variabile per selezionare l'elemento ul aggiunto nel file .html
+const shoppingListUl = document.querySelector('ul')
+
+// Creo una variabile per il ciclo while
+let i = 0;
+
+
+while (i < shoppingList.length) {
+    const shoppingElement = shoppingList[i]; // Dico al ciclo di creare una variabile con il nome di ciascun ingrediente
+    //console.log(shoppingElement);
+
+    const shoppingElementLi = document.createElement('li'); // Creo un elemento 'li'
+    shoppingElementLi.append(shoppingElement); // Collego l'ingrediente all'elemento 'li'
+    shoppingListUl.append(shoppingElementLi); // Collego l'elemento 'li' all'elemento 'ul' del file html
+
+    i++ //Incremento in valore dell'index
+
+}
